d05014414656cb0a827dbcc254f6ee4618690495
Riadh MNASRI <riadh.mnasri@yahoo.fr>
2016-11-26 23:20:46 +0100

    Add my-services  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyServicesComponent } from './my-services/my-services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyServicesComponent
   ],
   imports: [
     BrowserModule,
